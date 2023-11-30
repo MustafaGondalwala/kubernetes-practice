e5378d54d530eee0726fa0a926be0f8b6f4a25f2
Mustafa G <mustafa.gondalwala@ankercloud.com>
2023-11-30 07:47:11 +0100

    added backend

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,22 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL;
+    // Fetch data from the API
+    fetch(`${apiUrl}/api/greet`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGreeting(`Hello, ${data.username}!`);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // The empty dependency array ensures that the effect runs only once after the initial render
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{greeting}</p>
       </header>
     </div>
   );
